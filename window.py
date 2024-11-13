4fff1c66cbe86a20ed83bace021a111b8e60a630
Giira <euan.a.marsh@gmail.com>
2024-11-13 11:36:34 +0000

    Initial window class

---
+++ window.py
@@ -0,0 +1,27 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.root_widget = Tk()
+        self.root_widget.title = "Root Widget"
+        self.root_widget.protocol("WM_DELETE_WINDOW", self.close)
+
+        self.canvas = Canvas()
+        self.canvas.pack()
+
+        self.running = False
+
+
+    def redraw(self):
+        self.root_widget.update_idletasks()
+        self.root_widget.update()
+
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+    
+
+    def close(self):
+        self.running = False
