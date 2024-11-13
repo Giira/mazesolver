182fa20f566c1d94ffc18f86130eb32022732316
Giira <euan.a.marsh@gmail.com>
2024-11-13 14:18:53 +0000

    Point and Line

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from window import Window
+from window import Window, Point, Line
 
 win = Window(800, 600)
+
+point1 = Point(12, 12)
+point2 = Point(700, 500)
+line = Line(point1, point2)
+
+win.draw_line(line, "red")
+
 win.wait_for_close()
--- window.py
+++ window.py
@@ -6,7 +6,7 @@ class Window():
         self.root_widget.title = "Root Widget"
         self.root_widget.protocol("WM_DELETE_WINDOW", self.close)
 
-        self.canvas = Canvas()
+        self.canvas = Canvas(width=width, height=height)
         self.canvas.pack()
 
         self.running = False
@@ -25,3 +25,26 @@ class Window():
 
     def close(self):
         self.running = False
+
+    
+    def draw_line(self, Line, fill_colour):
+        Line.draw(self.canvas, fill_colour)
+
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    ## x=0 is the left limit
+    ## y=0 is the top limit
+
+
+class Line():
+    def __init__(self, point_1, point_2):
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+
+    def draw(self, Canvas, fill_colour):
+        Canvas.create_line(self.point_1.x, self.point_1.y, self.point_2.x, self.point_2.y, fill=fill_colour, width=2)
